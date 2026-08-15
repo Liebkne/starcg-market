44e0a96641422e4cc7b6cd852f653b44201dd43e
Liebkne <j50502314@gmail.com>
2026-08-15 09:33:37 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // 將這裡改成你的 Cloudflare Worker 網址。
 // 例如：https://starcg-market-search.你的帳號.workers.dev
-window.STARCG_API = "PASTE_YOUR_WORKER_URL_HERE";
+window.STARCG_API = "https://starcg-market-search.j50502314.workers.dev/";
